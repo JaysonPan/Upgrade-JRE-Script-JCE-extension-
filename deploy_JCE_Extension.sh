6d9996e58858e41d849e38d8944892cc5d45cbbc
Jason Pan <jaysonpan11@gmail.com>
2017-07-25 16:06:08 -0400

    commit 2 scripts

---
+++ deploy_JCE_Extension.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tar zxf jre*.tar.gz -C . 
+rm -rf jre*.tar.gz
+ln -sfn jre* java8
+unzip -qq jce_policy-8.zip
+mv UnlimitedJCEPolicyJDK8/*.jar "jre"*"/lib/security/"
+rm -rf jce_policy-8.zip UnlimitedJCEPolicyJDK8
+#java.security update
+sed -i 's/dev\/random/dev\/.\/urandom/'g jre*/lib/security/java.security
---
+++ get_latest_jre.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ext="tar.gz"
+jre_version=8
+
+readonly url="http://www.oracle.com"
+readonly jre_download_url1="$url/technetwork/java/javase/downloads/index.html"
+readonly jre_download_url2=$(curl -s $jre_download_url1 | egrep -o "\/technetwork\/java/\javase\/downloads\/jre${jre_version}-downloads-.+?\.html" | head -1 | cut -d '"' -f 1)
+[[ -z "$jre_download_url2" ]] && error "Could not get jre download url - $jre_download_url1"
+
+readonly jre_download_url3="${url}${jre_download_url2}"
+readonly jre_download_url4=$(curl -s $jre_download_url3 | egrep -o "http\:\/\/download.oracle\.com\/otn-pub\/java\/jdk\/[7-8]u[0-9]+\-(.*)+\/jre-[7-8]u[0-9]+(.*)linux-x64.$ext")
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" -N ${jre_download_url4[0]}
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip"
+
